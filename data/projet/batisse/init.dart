c30ea6a725643ce200c7249a9d5cc18e3b2907ab
David Curtis <curtis@linuxmint.linuxmint-domain>
2012-09-12 19:07:53 -0400

    init data error

--- data/projet/batisse/init.dart
+++ data/projet/batisse/init.dart
@@ -1,5 +1,15 @@
 // data/projet/batisse/init.dart 
  
 initProjetBatisse(var entries) { 
+  _initBatisses(entries); 
+ } 
+
+ _initBatisses(var entries) {
+   Batisses batisses = entries.batisses;
+   Concept batisseConcept = batisses.concept;
+
+   Batisse jeancoutubatisse = new Batisse(batisseConcept);
+   jeancoutubatisse.nom = 'Jean Coutu';
+   batisses.add(jeancoutubatisse);
 } 
  
